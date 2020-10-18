1cc49f8839cf22b232b4029ab94e1043c76d28a3
rhit-scheumdm <rhit-scheumdm@users.noreply.github.com>
2020-10-18 18:32:10 -0400

    initial stuff, app name

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,29 +3,11 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * PUT_YOUR_NAME_HERE
+ * Dylan Scheumann and Jack Speedy
  */
 
 /** namespace. */
 var rhit = rhit || {};
-
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
 
 /* Main */
 /** function and class syntax examples */
