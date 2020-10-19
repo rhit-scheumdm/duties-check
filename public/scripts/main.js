c64038c6193a151c117ef5087c9491548cc35e5e
speedyj1 <speedyj1@rose-hulman.edu>
2020-10-18 23:01:34 -0400

    log in page works

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,42 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * Dylan Scheumann and Jack Speedy
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/* Main */
-/** function and class syntax examples */
+rhit.logInCheck = class {
+	constructor() {
+		document.querySelector("#logInButton").addEventListener("click", (event) => {
+			const email = document.querySelector("#inputEmail").value;
+			const password = document.querySelector("#inputPassword").value;
+			console.log('email :>> ', email);
+			console.log('password :>> ', password);
+			firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+				window.location.href = "/duties.html";
+			})
+			.catch(function(error) {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				console.log("Existing account log in error", errorCode, errorMessage);
+			});
+		});
+	}
+
+}
+
+rhit.DutiesPage = class {
+	constructor() {
+		console.log('document.querySelector("#signOutButton") :>> ', document.querySelector("#signOutButton"));
+		document.querySelector("#signOutButton").addEventListener("click", (event) => {
+			window.location.href = "/";
+		});
+	}
+}
+
 rhit.main = function () {
 	console.log("Ready");
+	if(document.querySelector("#mainPage")) {
+		new rhit.logInCheck();
+	}
+	if(document.querySelector("#dutiesPage")) {
+		new rhit.DutiesPage();
+	}
 };
 
 rhit.main();
